cf5409a4afe0aea010d630f08bd073bcce28f956
jairdz04 <jairdz04@gmail.com>
2016-04-25 17:03:18 -0500

    Actualizacion1

---
+++ ResultadosUTB/Web/AdminPage.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ResultadosUTB.Modelo;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ResultadosUTB.Web.OnlyLog
+{
+    public partial class AdminPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserID"] != null)
+            {
+                Label1.Text = "Sesión iniciada :  " + Session["UserID"].ToString();
+            }
+            else
+            {
+                //Label1.Text = "Fail";
+               // Response.Write("<script>alert('Usted no ha iniciado sesión');</script>");
+                Response.Redirect("Default.aspx");
+            }
+
+            PnlEquipo.Visible = true;
+            PnlFecha.Visible = false;
+            PnlResultado.Visible = false;
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session.Remove("UserID");
+            Session.RemoveAll();
+            Response.Redirect("Default.aspx");
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            PnlEquipo.Visible = true;
+            PnlFecha.Visible = false;
+            PnlResultado.Visible = false;
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            PnlEquipo.Visible = false;
+            PnlFecha.Visible = true;
+            PnlResultado.Visible = false;
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            PnlEquipo.Visible = false;
+            PnlFecha.Visible = false;
+            PnlResultado.Visible = true;
+        }
+    }
+}
---
+++ ResultadosUTB/Web/Verification.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ResultadosUTB.Modelo;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ResultadosUTB.Web
+{
+    public partial class Verification : System.Web.UI.Page
+    {
+
+        ConexionBl a = new ConexionBl();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            //  a.getA.Open();
+            //SqlDataAdapter sda = new SqlDataAdapter("Select count(*) From Usuarios Where ds_login '" + TextBox1.Text , a.getA);
+            //DataTable dt = new DataTable();
+            /*sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                 Session["UserID"] = TextBox1.Text;
+                Response.Redirect("SignUp.aspx");
+
+            }
+            else
+            {
+               
+                Response.Write("<script>alert('codigo erroneo');</script>");
+                
+            }
+
+            a.getA.Close();
+        }*/
+            Response.Redirect("SignUp.aspx");
+        
+        }
+
+
+        }
+    }
+
+
