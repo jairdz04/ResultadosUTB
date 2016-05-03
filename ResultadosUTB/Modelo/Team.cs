032124407b981c09dd20f3fe02ae3294c7adac86
jairdz04 <jairdz04@gmail.com>
2016-05-03 09:50:22 -0500

    Update Team.cs

--- ResultadosUTB/Modelo/Team.cs
+++ ResultadosUTB/Modelo/Team.cs
@@ -12,7 +12,6 @@ namespace ResultadosUTB.Modelo
 
         public string Nombre;
         public string Cod;
-        public string Name_Contact;
         public string E_mail_Contact;
         public string Tel_contact;
 
@@ -32,11 +31,6 @@ namespace ResultadosUTB.Modelo
         {
             get { return Cod; }
             set { Cod = value; }
-        }
-        public string name_contact
-        {
-            get { return Name_Contact; }
-            set { Name_Contact = value; }
         }
 
 
@@ -61,16 +55,14 @@ namespace ResultadosUTB.Modelo
       public Team () {
             this.Nombre = "Jair";
             this.Cod = "123";
-             this.Name_Contact = "Jose Joaquin";
         this.E_mail_Contact = "Jairdz040";
        this.Tel_contact = "000000";
         }
 
-public Team (string Nombre, string Cod , string Name_Contact, string E_mail_Contact, string Tel_contact){
+public Team (string Nombre, string Cod , string E_mail_Contact, string Tel_contact){
     
      this.Nombre = Nombre;
             this.Cod = Cod ;
-             this.Name_Contact = Name_Contact;
         this.E_mail_Contact = E_mail_Contact ;
        this.Tel_contact = Tel_contact;
     
@@ -83,7 +75,6 @@ public Team (string Nombre, string Cod , string Name_Contact, string E_mail_Cont
         public override string ToString(){
             return "Nombre: " + this.Nombre +
             "\nCodigo : " + this.Cod +
-            "\nNombre de contacto : " + this.Name_Contact +
             "\nE Mail de contacto : " + this.E_mail_Contact +
             "\nTelefono de contacto : " + this.Tel_contact ;
             
@@ -101,7 +92,6 @@ public Team (string Nombre, string Cod , string Name_Contact, string E_mail_Cont
             
             if ((this.Nombre == o.Nombre)&& 
             (this.Cod == o.Cod) &&
-            (this.Name_Contact == o.Name_Contact) &&
             (this.E_mail_Contact == o.E_mail_Contact) &&
             (this.Tel_contact == o.Tel_contact))
             
